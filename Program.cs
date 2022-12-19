5b3aa1f5d0486a3009691fd38ff5999315b7a0d5
Vinicius <Vinicius@DESKTOP-5SQQP23>
2022-12-19 08:04:45 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System; 
+using DIO.Bank.Models;
+
+namespace DIO.Bank
+{
+    class Program{
+        static void Main(string[] args){
+            Conta minhaConta = new Conta(DiO.Bank.Models.TipoConta.PessoaFisica, 0, 0, "Vinicius Henrique");
+
+            Console.WriteLine(minhaConta.ToString());
+        }
+
+        //private static string ObterOpcaoUsuario()
+        //{
+        //    Console.WriteLine();
+        //}
+    }
+}
